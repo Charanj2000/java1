996f45c4005b37746d85a654d964a9abc6ee6702
Charanj2000 <85435081+Charanj2000@users.noreply.github.com>
2021-10-12 13:24:40 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+package geekster;
+
+public class Helloworld {
+    public static void main(String args[]) { // function/method
+        System.out.println("Helloworld!");
+        System.out.println("Welcome CJ");
+        System.out.print("Java is a good langauge");
+    }
+}
---
+++ oddeven.java
@@ -0,0 +1,17 @@
+package geekster;
+
+public class oddeven {
+    public static void main (String args[]) {
+
+        int num = 13;
+
+        if (num % 2 == 0) {
+            System.out.print("Even number");
+        }else {
+            System.out.print("Odd number");
+        }
+
+
+
+    }
+ }
